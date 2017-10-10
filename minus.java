bacc8932e6b8437456f634ad397e914447c01325
Ospanova Assida <32270013+Assidka@users.noreply.github.com>
2017-10-10 09:00:28 +0600

    Create minus.java

---
+++ minus.java
@@ -0,0 +1,6 @@
+//name:Assida Ospanova
+//group:3eno4d
+public class minus{
+public static void main(String args[]){
+	System.out.println("Java:"+args[0]+"-"+args[1]+"="+(Integer.parseint(args[0])-Integer.parseint(args[1])));}
+}
